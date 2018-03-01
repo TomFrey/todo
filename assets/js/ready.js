99e18bc1d313fe66915765ca63441d4abfb81376
Thomas Frey <joe@mitlinxlernen.ch>
2018-03-01 07:08:35 +0100

    alles in Module gepackt

--- assets/js/ready.js
+++ assets/js/ready.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function ready(cb) {
-// ready
+    //Wenn der DOM geladen ist, dann die übergebene Funktion cb aufrufen
     if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
         cb();
     } else {
--- assets/js/storing.js
+++ assets/js/storing.js
@@ -1,44 +1,59 @@
-/**
- * Speichert das array mit allen todoItems im local store des Browsers
- *
- * @param todoListModel - array mit todoItems
- */
-function storeTodoList(todoListModel){
-    let jsonList = JSON.stringify(todoListModel.tasks);
+'use strict';
 
-    console.log('a: ' + jsonList);
+var Store = (function(){
 
-    localStorage.setItem('todoList', jsonList);
-}
-
-
-/**
- * Holt die gespeicherte TodoListe aus dem store des Browsers.
- * Wenn es noch nichts im Speicher gibt, wird eine leere Liste
- * zurück gegeben.
- *
- * @returns {TodoList|*}
- */
-function getTodoListFromStore(){
-    let todosAsJson = localStorage.getItem('todoList');
-    let todoListModel = new TodoList();
-
-    //Daten sind schon vorhanden
-    if(todosAsJson){
-        let todosAsArrayOfObjects = JSON.parse(todosAsJson);
-
-        todosAsArrayOfObjects.forEach(function(todo, index){
-            todoListModel.addTask(todo._text);
-            //todo checked unchecked aus dem Speicher lesen, ev Model anpassen
-            //noch testen
-            if(todo.erledigt){
-                todoListModel.checkTask(index);
-            }
-        });
-
-        return todoListModel;
-    } else {
-        return todoListModel;
+    /**
+     * Speichert das array mit allen todoItems im local store des Browsers
+     *
+     * @param todoListModel - array mit todoItems
+     */
+    function storeTodoList(todoListModel){
+        let jsonList = JSON.stringify(todoListModel.tasks);
+        localStorage.setItem('todoList', jsonList);
     }
-}
+
+
+    /**
+     * Holt die gespeicherte TodoListe aus dem store des Browsers.
+     * Wenn es noch nichts im Speicher gibt, wird eine leere Liste
+     * zurück gegeben.
+     *
+     * @returns {TodoList|*}
+     */
+    function getTodoListFromStore(){
+        let todosAsJson = localStorage.getItem('todoList');
+        let todoListModel = new TodoList();
+
+        //Daten sind schon vorhanden
+        if(todosAsJson){
+            let todosAsArrayOfObjects = JSON.parse(todosAsJson);
+
+            todosAsArrayOfObjects.forEach(function(todo, index){
+                todoListModel.addTask(todo._text);
+                if(todo.erledigt){
+                    todoListModel.checkTask(index);
+                }
+            });
+            return todoListModel;
+
+            //noch keine Daten vorhanden, leere Liste zurück geben
+        } else {
+            return todoListModel;
+        }
+    }
+
+
+    //Public api
+    return {
+        saveAll: storeTodoList,
+        getAllTodos: getTodoListFromStore
+    };
+
+})();
+
+
+
+
+
+
 
